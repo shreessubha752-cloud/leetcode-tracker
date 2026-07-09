7c8998adeca837fcc897d8a982753e58ed7037e9
shreessubha752-cloud <shreessubha752@gmail.com>
2026-07-09 10:09:24 +0530

    Create 17-LetterCombinationsOfAPhoneNumber

---
+++ 17-LetterCombinationsOfAPhoneNumber/17-LetterCombinationsOfAPhoneNumber.py
@@ -0,0 +1,27 @@
+# Last updated: 09/07/2026, 10:09:24
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+
+        # Mapping of digits to letters
+        phone_map = {
+            '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+            '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
+        }
+
+        result = []
+
+        def backtrack(index, path):
+            if index == len(digits):
+                result.append(path)
+                return
+            for char in phone_map[digits[index]]:
+                backtrack(index + 1, path + char)
+
+        backtrack(0, "")
+        return result
