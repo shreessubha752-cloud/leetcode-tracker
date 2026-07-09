155c961ce7e4eddf20a196c37e215e9af851ba4e
shreessubha752-cloud <shreessubha752@gmail.com>
2026-07-09 10:09:33 +0530

    Create 12-IntegerToRoman

---
+++ 12-IntegerToRoman/12-IntegerToRoman.py
@@ -0,0 +1,16 @@
+# Last updated: 09/07/2026, 10:09:33
+class Solution:
+    def intToRoman(self, num):
+        val_to_roman = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'),  (90, 'XC'), (50, 'L'),  (40, 'XL'),
+            (10, 'X'),   (9, 'IX'),  (5, 'V'),   (4, 'IV'),
+            (1, 'I')
+        ]
+
+        result = ""
+        for value, symbol in val_to_roman:
+            while num >= value:
+                result += symbol
+                num -= value
+        return result
