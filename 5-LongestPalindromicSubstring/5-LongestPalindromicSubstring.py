6560cf8bfa07914eacdf410c2a793f7d1e57f36d
shreessubha752-cloud <shreessubha752@gmail.com>
2026-07-09 10:09:45 +0530

    Create 5-LongestPalindromicSubstring

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -0,0 +1,24 @@
+# Last updated: 09/07/2026, 10:09:46
+class Solution:
+    def longestPalindrome(self, s):
+        if not s or len(s) == 1:
+            return s
+
+        start, end = 0, 0
+
+        for i in range(len(s)):
+            len1 = self._expandFromCenter(s, i, i)       # Odd-length palindrome
+            len2 = self._expandFromCenter(s, i, i + 1)   # Even-length palindrome
+            max_len = max(len1, len2)
+
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+
+        return s[start:end + 1]
+
+    def _expandFromCenter(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
