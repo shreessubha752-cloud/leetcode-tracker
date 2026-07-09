f80113ac0d4c17142e08f5e8d117e78b37a9acb8
shreessubha752-cloud <shreessubha752@gmail.com>
2026-07-09 10:08:54 +0530

    Create 34-FindFirstAndLastPositionOfElementInSortedArray

---
+++ 34-FindFirstAndLastPositionOfElementInSortedArray/34-FindFirstAndLastPositionOfElementInSortedArray.py
@@ -0,0 +1,35 @@
+# Last updated: 09/07/2026, 10:08:55
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        def findLeft():
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return left
+
+        def findRight():
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return right
+
+        left = findLeft()
+        right = findRight()
+
+        if left <= right and right < len(nums) and nums[left] == target and nums[right] == target:
+            return [left, right]
+        else:
+            return [-1, -1]
