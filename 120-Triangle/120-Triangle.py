6a2252f9099ad00383634037413ae97bafd91a83
shreessubha752-cloud <shreessubha752@gmail.com>
2026-07-09 10:08:44 +0530

    Create 120-Triangle

---
+++ 120-Triangle/120-Triangle.py
@@ -0,0 +1,7 @@
+# Last updated: 09/07/2026, 10:08:45
+class Solution(object):
+    def minimumTotal(self, triangle):
+        for row in range(len(triangle) - 2, -1, -1):
+            for col in range(len(triangle[row])):
+                triangle[row][col] += min(triangle[row+1][col], triangle[row+1][col+1])
+        return triangle[0][0]
