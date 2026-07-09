cf0db82a5d374ecf21f588a79eae8e2ae677a303
shreessubha752-cloud <shreessubha752@gmail.com>
2026-07-09 10:08:58 +0530

    Create 32-LongestValidParentheses

---
+++ 32-LongestValidParentheses/32-LongestValidParentheses.py
@@ -0,0 +1,22 @@
+# Last updated: 09/07/2026, 10:08:58
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        max_len = 0
+        stack = [-1]  # Initialize with -1 to handle edge cases
+
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_len = max(max_len, i - stack[-1])
+
+        return max_len
+        
