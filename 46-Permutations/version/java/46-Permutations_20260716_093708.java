b91dd7ba28f3a781f32f6bac88156801b1d51925
shreessubha752-cloud <shreessubha752@gmail.com>
2026-07-16 09:37:09 +0530

    Create 46-Permutations

---
+++ 46-Permutations/version/java/46-Permutations_20260716_093708.java
@@ -0,0 +1,21 @@
+// Last updated: 16/07/2026, 09:37:08
+1import java.util.*;
+2
+3class Solution {
+4    public List<List<String>> groupAnagrams(String[] strs) {
+5        Map<String, List<String>> map = new HashMap<>();
+6        
+7        for (String s : strs) {
+8            // Sort characters to form the key
+9            char[] chars = s.toCharArray();
+10            Arrays.sort(chars);
+11            String key = new String(chars);
+12            
+13            // Add to the group
+14            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+15        }
+16        
+17        return new ArrayList<>(map.values());
+18    }
+19}
+20
