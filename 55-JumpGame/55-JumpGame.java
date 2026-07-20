e9d6203b3d60226e89aa2929f3d2b3e2dd043d25
shreessubha752-cloud <shreessubha752@gmail.com>
2026-07-20 10:13:34 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,15 @@
+// Last updated: 20/07/2026, 10:13:33
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false; // can't reach this index
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+        
+        return true;
+    }
+}
