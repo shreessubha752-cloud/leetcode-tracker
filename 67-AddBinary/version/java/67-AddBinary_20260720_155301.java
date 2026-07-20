e48c3fcdc6c17f0401a98a075961160684ea5d5b
shreessubha752-cloud <shreessubha752@gmail.com>
2026-07-20 15:53:02 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/version/java/67-AddBinary_20260720_155301.java
@@ -0,0 +1,19 @@
+// Last updated: 20/07/2026, 15:53:01
+1class Solution {
+2    public String addBinary(String a, String b) {
+3        StringBuilder sb = new StringBuilder();
+4        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+5
+6        while (i >= 0 || j >= 0 || carry > 0) {
+7            int sum = carry;
+8            if (i >= 0) sum += a.charAt(i--) - '0';
+9            if (j >= 0) sum += b.charAt(j--) - '0';
+10
+11            sb.append(sum % 2);   // current bit
+12            carry = sum / 2;      // update carry
+13        }
+14
+15        return sb.reverse().toString();
+16    }
+17}
+18
